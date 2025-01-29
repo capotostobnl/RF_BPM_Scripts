f7fb35f2ad3451e79d0a610051e5eac3aa431272
capotostobnl <109634711+capotostobnl@users.noreply.github.com>
2025-01-29 15:20:47 -0500

    Update bpm_status.py
    Restructured code, added ALL BPM Functionality.

--- bpm_status.py
+++ bpm_status.py
@@ -1,113 +1,136 @@
 """Display BPM Status
-Updated to Python3 Syntax 1/31/2024 M. Capotosto"""
-#!/usr/bin/python3
+1/29/2025 M. Capotosto"""
+# !/usr/bin/python3
 
-#from bpmclass import bpm
 import sys
 import time
-from select import select
 from threading import Thread
-from bpmclass import *
-from bpm_addresses import *
+from bpmclass import Bpm, print_status_header
+from bpm_addresses import linbpm, ltbbpm, boobpm, bstbpm, \
+    c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, c12, c13, \
+    c14, c15, c16, c17, c18, c19, c20, c21, c22, c23, c24, \
+    c25, c26, c27, c28, c29, c30 # noqa
 
 
+def main():
+    if len(sys.argv) < 2:
+        print("Usage:  bpm_status [linac ltb booster sr [cell] all]")
+        sys.exit(0)
 
+    area = ["linac", "ltb", "booster", "bst", "sr", "all"]
+    cellnum = 0
 
-
-
-
-if len(sys.argv) < 2:
-    print ("Usage:  bpm_status [linac ltb booster sr [cell]]")
-    sys.exit(0)
-
-area = ["linac","ltb","booster","bst","sr"]
-
-if sys.argv[1] in area:
-    if sys.argv[1] == "linac":
-        bpmlist = linbpm
-    elif sys.argv[1] == "ltb":
-        bpmlist = ltbbpm
-    elif sys.argv[1] == "booster":
-        bpmlist = boobpm
-    elif sys.argv[1] == "bst":
-        bpmlist = bstbpm
-    elif sys.argv[1] == "sr":
-        if len(sys.argv) == 3:
-            cellnum = int(sys.argv[2])
-            if cellnum > 0 and cellnum < 33:
-                #print "Cell Number : %d" % cellnum
-                bpmlist = globals()['c'+ sys.argv[2]] #csrbpm
+    if sys.argv[1] in area:
+        if sys.argv[1] == "linac":
+            drawOutputs("LINAC", 0, linbpm)
+        elif sys.argv[1] == "ltb":
+            drawOutputs("LTB", 0, ltbbpm)
+        elif sys.argv[1] == "booster":
+            drawOutputs("BOOSTER", 0, boobpm)
+        elif sys.argv[1] == "bst":
+            drawOutputs("BST", 0, bstbpm)
+        elif sys.argv[1] == "sr":
+            if len(sys.argv) == 3:
+                cellnum = int(sys.argv[2])
+                if cellnum > 0 and cellnum < 33:
+                    # print "Cell Number : %d" % cellnum
+                    drawOutputs("SR", cellnum, globals()['c' + sys.argv[2]])
+                else:
+                    print("Invalid Cell Number")
+                    sys.exit(0)
             else:
-                print ("Invalid Cell Number")
+                print("For SR, also enter Cell Number")
                 sys.exit(0)
-        else:
-            print ("For SR, also enter Cell Number")
-            sys.exit(0)
+        elif sys.argv[1] == "all":
+            cellnum = 1
+            while (1):
+                print("\n\n---------------------------------------------------\
+                      ---------------------------------------------------\n\n")
+                drawOutputs("LINAC", 0, linbpm)
+                print("\n\n----------------------------------------------------\
+                      ---------------------------------------------------\n\n")
+                drawOutputs("LTB", 0, ltbbpm)
+                print("\n\n-----------------------------------------------------\
+                      --------------------------------------------------\n\n")
+                drawOutputs("BOOSTER", 0, boobpm)
+                print("\n\n------------------------------------------------------\
+                      ---------------------------------------------------\n\n")
+                drawOutputs("BST", 0, bstbpm)
+                print("\n\n------------------------------------------------------\
+                      ---------------------------------------------------\n\n")
+                for cellnum in range(1, 31):
+                    drawOutputs("SR", cellnum, globals()['c' + str(cellnum)])
+                    print("\n\n---------------------------------------------------\
+                        --------------------------------------------------------\
+                          --\n\n")
+                    cellnum += cellnum
+                break
+    else:
+        print("Usage:  bpm_status [linac ltb booster bst sr]")
+        sys.exit(0)
 
-else:
-    print ("Usage:  bpm_status [linac ltb booster bst sr]")
-    sys.exit(0)
-
-print ("Area = ",sys.argv[1])
-if sys.argv[1] == "sr":
-    print ("Cell = ", cellnum)
-
-
-#list of bpm objects
-b = []
-
-
-#print "BPM's in list %s" % len(bpmlist)
-
-# create objects
-for bpm in range(len(bpmlist)):
-    b.append(Bpm(bpmlist[bpm]))
-#print "objects created"
-
-# connect to moxa
-#for bpm in range(len(bpmlist)):    b[bpm].moxa_connect()
-#print "moxa's connected"
-# Connect to moxa (seperate threads to speed things up)
-threads = []
-for bpm in range(len(bpmlist)):
-    t = Thread(target=b[bpm].moxa_connect)
-    t.start()
-    threads.append(t)
-# wait for all threads to complete before continuing
-for i in threads:
-    i.join()
-
-# Connect to sockets (seperate threads to speed things up)
-threads = []
-for bpm in range(len(bpmlist)):
-    t = Thread(target=b[bpm].ip_connect)
-    t.start()
-    threads.append(t)
-# wait for all threads to complete before continuing
-for i in threads:
-    i.join()
-#print "ip's connected"
-
-# get status
-threads = []
-for bpm in range(len(bpmlist)):
-    t = Thread(target=b[bpm].get_status,args=(bpm,))
-    threads.append(t)
-    t.start()
-    time.sleep(0.1) #Delay to allow time for CAGet operation
-#wait for all threads to complete
-for i in threads:
-    i.join()
-
-#print "got status"
-
-#print status
-print_status_header()
-for bpm in range(len(bpmlist)):
-    print (b[bpm].stat)
+#    drawOutputs(cellnum, bpmlist)  # draw the formatted output status table
 
 
-# without threads
-#for bpm in range(0,len(bpmlist)):    b[bpm].ip_connect()
-#
-#print_status()
+def drawOutputs(selectedArea, cellnum, bpmlist):
+    printAreaHeader(selectedArea, cellnum)
+    b = connectBPM(bpmlist)
+    getStatus(bpmlist, b)
+
+
+def printAreaHeader(selectedArea, cellnum):
+    print("Area = ", selectedArea)
+    if cellnum > 0:
+        print("Cell = ", cellnum)
+
+
+def connectBPM(bpmlist):
+    # list of bpm objects
+    b = []
+
+    # create objects
+    for bpm in range(len(bpmlist)):
+        b.append(Bpm(bpmlist[bpm]))
+
+    # Connect to moxa (seperate threads to speed things up)
+    threads = []
+    for bpm in range(len(bpmlist)):
+        t = Thread(target=b[bpm].moxa_connect)
+        t.start()
+        threads.append(t)
+    # wait for all threads to complete before continuing
+    for i in threads:
+        i.join()
+
+    # Connect to sockets (seperate threads to speed things up)
+    threads = []
+    for bpm in range(len(bpmlist)):
+        t = Thread(target=b[bpm].ip_connect)
+        t.start()
+        threads.append(t)
+    # wait for all threads to complete before continuing
+    for i in threads:
+        i.join()
+    return b
+
+
+def getStatus(bpmlist, b):
+    # get status
+    threads = []
+    for bpm in range(len(bpmlist)):
+        t = Thread(target=b[bpm].get_status, args=(bpm,))
+        threads.append(t)
+        t.start()
+        time.sleep(0.1)  # Delay to allow time for CAGet operation
+    # wait for all threads to complete
+    for i in threads:
+        i.join()
+
+    # print status
+    print_status_header()
+    for bpm in range(len(bpmlist)):
+        print(b[bpm].stat)
+
+
+if __name__ == "__main__":
+    main()
