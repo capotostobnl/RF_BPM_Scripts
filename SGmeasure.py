52837185e11ed3743ee254b2ca40ee2ec39afdf9
Michael Capotosto <capotosto@bnl.gov>
2026-05-15 20:09:38 -0400

    Update SGC Scripts to add C09 ID BPMs, C29 ID BPMs to repo

--- SGmeasure.py
+++ SGmeasure.py
@@ -32,15 +32,15 @@ if not os.path.exists("Data"):
 #  cell[01,02,03,04,05,06,07,08,09,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30]
 # Nbpm = [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 9, 0, 0, 0, 0, 0, 0,
 #         0, 0, 0, 0, 0, 0,0]
-Nbpm = [6, 8, 8, 10, 9, 6, 10, 8, 6, 8, 8, 10, 6, 6, 6, 8, 9, 8, 10, 8, 8, 6,
-        9, 6, 6, 6, 8, 8, 6, 10]
+Nbpm = [6, 8, 8, 10, 9, 6, 10, 8, 8, 8, 8, 10, 6, 6, 6, 8, 9, 8, 10, 8, 8, 6,
+        9, 6, 6, 6, 8, 8, 10, 10]
 #  The mask below allows the user to mask off any BPM from this script.  For
 #  example, for Cell 1 if all 6 BPMs are to be scanned the mask would be 0x3F
 #  but if you want to disable BPM3 then the
 #  mask would be 0x3B (b111011)
 
 #  cell[  1      2      3      4      5      6      7      8      9     10     11     12     13     14     15  ]  # noqa: E501
-Mask = [0x03F, 0x0FF, 0x0FF, 0x3FF, 0x1FF, 0x03F, 0x3FF, 0x0FF, 0x03F, 0x0FF, 0x0FF, 0x3FF, 0x03F, 0x03F, 0x03F,  # noqa: E501
+Mask = [0x03F, 0x0FF, 0x0FF, 0x3FF, 0x1FF, 0x03F, 0x3FF, 0x0FF, 0x0FF, 0x0FF, 0x0FF, 0x3FF, 0x03F, 0x03F, 0x03F,  # noqa: E501
         0x0FF, 0x1FF, 0x0FF, 0x3FF, 0x0FF, 0x0FF, 0x03F, 0x1FF, 0x03F, 0x03F, 0x03F, 0x0FF, 0x0FF, 0x03F, 0x3FF]  # noqa: E501
 #  cell[  16     17     18     19     20     21     22     23     24     25     26     27     28     29     30 ]  # noqa: E501
 
